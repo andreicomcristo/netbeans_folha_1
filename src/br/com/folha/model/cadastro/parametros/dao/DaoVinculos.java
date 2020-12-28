969879c2417631e94d9a3e0266c3907a93596efa
andre <andre@andre-IPMH110P>
2020-12-28 04:31:15 -0300

    Implementando a tela de vínculos

--- src/br/com/folha/model/cadastro/parametros/dao/DaoVinculos.java
+++ src/br/com/folha/model/cadastro/parametros/dao/DaoVinculos.java
@@ -1,14 +1,162 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
+ * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
+
 package br.com.folha.model.cadastro.parametros.dao;
+
+import br.com.folha.model.cadastro.parametros.bean.BeanCargaHorariaSemanal;
+import br.com.folha.util.ConnectionFactory;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.JOptionPane;
 
 /**
  *
- * @author andre
+ * @author andrei
  */
-public class DaoVinculos {
+public class DaoVinculos { 
+
+     Connection con = null;
+
+    public boolean inserirCargaHorariaSemanal(BeanCargaHorariaSemanal cargaHorariaSemanal){
+                boolean executou = false;
+		try {
+			con = ConnectionFactory.getConnection();
+                                                                            // nome da tebela
+			PreparedStatement stmt = con.prepareStatement("insert into public.carga_horaria_semanal (carga_horaria, descricao_carga_horaria ) values ( ?,? )");
+
+            stmt.setInt(1, cargaHorariaSemanal.getCargaHoraria());
+            stmt.setString(2, cargaHorariaSemanal.getDescricaoCargaHoraria());
+
+			stmt.execute();
+			stmt.close();
+
+			con.close();
+                        executou = true;
+                        
+		} catch (Exception e) {
+                    executou = false;
+                    JOptionPane.showMessageDialog(null, e.getMessage());
+			e.printStackTrace();
+		} finally{
+
+			try{
+				con.close();
+
+			}catch (SQLException e){JOptionPane.showMessageDialog(null, e.getMessage());
+			}
+                }
+    return executou;    
+    }
     
+    public boolean excluirCargaHorariaSemanal(BeanCargaHorariaSemanal cargaHorariaSemanal){
+                boolean executou = false;
+		try {
+			con = ConnectionFactory.getConnection();
+                                                                            // nome da tebela
+			PreparedStatement stmt = con.prepareStatement("DELETE FROM public.carga_horaria_semanal where public.carga_horaria_semanal.seq_carga_horaria_sem = ? ");
+
+                        stmt.setLong(1, cargaHorariaSemanal.getSeqCargaHorariaSemanal());
+
+			stmt.execute();
+			stmt.close();
+
+			con.close();
+                        executou = true;
+
+		} catch (Exception e) {
+                    executou = false;
+                    JOptionPane.showMessageDialog(null, e.getMessage());
+			e.printStackTrace();
+		} finally{
+
+			try{
+				con.close();
+
+			}catch (SQLException e){JOptionPane.showMessageDialog(null, e.getMessage());
+			}
+                }
+    return executou;    
+    }
+
+    public boolean alterarCargaHorariaSemanal(BeanCargaHorariaSemanal cargaHorariaSemanal){
+                boolean executou = false;
+		try {
+			con = ConnectionFactory.getConnection();
+                                                                            // nome da tebela
+			PreparedStatement stmt = con.prepareStatement("UPDATE public.carga_horaria_semanal set carga_horaria = ?, descricao_carga_horaria = ?  where public.carga_horaria_semanal.seq_carga_horaria_sem = ? ");
+
+                        stmt.setInt(1, cargaHorariaSemanal.getCargaHoraria());
+                        stmt.setString(2, cargaHorariaSemanal.getDescricaoCargaHoraria());
+                        stmt.setLong(3, cargaHorariaSemanal.getSeqCargaHorariaSemanal());
+
+			stmt.execute();
+			stmt.close();
+
+			con.close();
+                        executou = true;
+
+		} catch (Exception e) {
+                    executou = false;
+                    JOptionPane.showMessageDialog(null, e.getMessage());
+			e.printStackTrace();
+		} finally{
+
+			try{
+				con.close();
+
+			}catch (SQLException e){JOptionPane.showMessageDialog(null, e.getMessage());
+			}
+                }
+    return executou;    
+    }
+         
+    public List<BeanCargaHorariaSemanal> selecionarCargaHorariaSemanal() {
+
+        List<BeanCargaHorariaSemanal> listaConsulta = new ArrayList<BeanCargaHorariaSemanal>();
+
+     try {
+
+
+       con = ConnectionFactory.getConnection();
+
+         try {
+
+             PreparedStatement stmt = con.prepareStatement("select seq_carga_horaria_sem, carga_horaria, descricao_carga_horaria from public.carga_horaria_semanal order by carga_horaria");
+
+              
+
+             ResultSet rs = stmt.executeQuery();
+
+             while(rs.next()) {
+
+                   long seq_carga_horaria_sem = rs.getLong("seq_carga_horaria_sem");
+                   int carga_horaria = rs.getInt("carga_horaria");
+                   String descricao_carga_horaria = rs.getString("descricao_carga_horaria");
+
+                 listaConsulta.add(new BeanCargaHorariaSemanal(seq_carga_horaria_sem, carga_horaria, descricao_carga_horaria));
+
+             }
+
+         } finally {
+             try {
+                 con.close();
+             } catch (SQLException e) {JOptionPane.showMessageDialog(null, e.getMessage());
+                 e.printStackTrace();
+             }
+         }
+     } catch (SQLException e) {JOptionPane.showMessageDialog(null, e.getMessage());
+         e.printStackTrace();
+     }
+
+     return listaConsulta;
+
+ }
+
+
 }
