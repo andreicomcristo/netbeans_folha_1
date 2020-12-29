675ebdd085f533e71c188c5127e6761c51291aac
andre <andre@andre-IPMH110P>
2020-12-29 08:53:30 -0300

    Iplementando a tela de cadastro de privilégios

---
+++ src/br/com/folha/model/cadastro/parametros/bean/BeanPrivilegios.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.folha.model.cadastro.parametros.bean;
+
+/**
+ *
+ * @author andre
+ */
+public class BeanPrivilegios {
+    
+    private long seqPrivilegio;
+    private String nomePrivilegio;
+    private String descricaoPrivilegio;
+
+    public BeanPrivilegios() {
+    }
+
+    public BeanPrivilegios(long seqPrivilegio, String nomePrivilegio, String descricaoPrivilegio) {
+        this.seqPrivilegio = seqPrivilegio;
+        this.nomePrivilegio = nomePrivilegio;
+        this.descricaoPrivilegio = descricaoPrivilegio;
+    }
+    
+    
+
+    public long getSeqPrivilegio() {
+        return seqPrivilegio;
+    }
+
+    public void setSeqPrivilegio(long seqPrivilegio) {
+        this.seqPrivilegio = seqPrivilegio;
+    }
+
+    public String getNomePrivilegio() {
+        return nomePrivilegio;
+    }
+
+    public void setNomePrivilegio(String nomePrivilegio) {
+        this.nomePrivilegio = nomePrivilegio;
+    }
+
+    public String getDescricaoPrivilegio() {
+        return descricaoPrivilegio;
+    }
+
+    public void setDescricaoPrivilegio(String descricaoPrivilegio) {
+        this.descricaoPrivilegio = descricaoPrivilegio;
+    }
+    
+    
+    
+}
